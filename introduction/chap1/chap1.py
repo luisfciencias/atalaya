19ee4aa02027db67b8d061b6c474f77bb22f7825
Luis Torres <luisfciencias@gmail.com>
2017-11-15 17:19:02 +1100

    NEW: Add input tools MNIST data

--- introduction/chap1/chap1.py
+++ introduction/chap1/chap1.py
@@ -1,1 +1,30 @@
 # source code for the implementation of a NN for digit recognition
+
+# tools
+from tools_chap1 import load_mnist
+import numpy as np
+
+# reading the mnist data set, each term is a tuple for X, y
+train_set, valid_set ,test_set = load_mnist()
+
+X_train = train_set[0]
+y_train = train_set[1]
+
+X_valid = valid_set[0]
+y_valid = valid_set[1]
+
+X_test = test_set[0]
+y_test = test_set[1]
+
+print('The shapes of the mnist arrays are:')
+print('Training:')
+print('X:',X_train.shape)
+print('y:',y_train.shape)
+
+print('Validation:')
+print('X:',X_valid.shape)
+print('y:',y_valid.shape)
+
+print('Testing:')
+print('X:',X_test.shape)
+print('y:',y_test.shape)
--- introduction/chap1/tools_chap1.py
+++ introduction/chap1/tools_chap1.py
@@ -1,1 +1,21 @@
 # tools for the source code in chapter 1
+import pickle as cPickle
+import gzip
+
+# load the mnist data set
+# reading the pickle file with the format from 
+# http://deeplearning.net/data/mnist/mnist.pkl.gz
+def load_mnist():
+    ''' 
+    read the pickle file from the mnist data set and outputs the 
+    numpy arrays for training, testing and validation. Respectively: 
+    X = shape(50000, 784) , shape(10000, 784) , shape(10000, 784)
+    y = shape(50000,) , shape(10000,) , shape(10000,)
+    '''
+    path2mnist = '../../data/mnist.pkl.gz'
+    f = gzip.open(path2mnist, 'rb')
+    # it was necessary to include the encoding flag 'latin1' for py3
+    train_set, valid_set, test_set = cPickle.load(f, encoding='latin1')
+    f.close()
+    return train_set, valid_set, test_set
+
